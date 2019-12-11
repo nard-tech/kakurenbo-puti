4d1d2e95e45ced0910d73e16a0e4ca32e75a997d
Shu Fujita <s.fujita@nard.tech>
2019-12-11 21:27:34 +0900

    Fix 'active_record_model_spec.rb'

--- spec/kakurenbo_puti/active_record_model_spec.rb
+++ spec/kakurenbo_puti/active_record_model_spec.rb
@@ -9,9 +9,7 @@ describe '#define_active_record_model' do
     expect(Object.const_defined? :Drink).to be_truthy
   end
 
-  describe 'created class' do
-    it 'has methods of ActiveRecord.' do
-      expect(Drink).to respond_to(:create, :find, :where, :update_all)
-    end
+  it 'has methods of ActiveRecord.' do
+    expect(Drink).to respond_to(:create, :find, :where, :update_all)
   end
 end
